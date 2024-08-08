0d4f861d496869b03e706e88c6efa2767653f549
fremmede <you@example.com>
2024-08-08 18:08:22 -0500

    Update file

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(destPotJson)) {
     console.log('pot.json copied to the project root.');
 } else {
     // If it already exists, show a message
-   console.log('pot.json already exists in the project root. No action taken.');
+    console.log('pot.json already exists in the project root. No action taken.');
 }
 
 // Check if Gulpfile.js already exists at the destination
